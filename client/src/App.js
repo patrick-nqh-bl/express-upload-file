10c109ef6e7d2f1918cd753d122cf4223a2bf96e
Patrick <patrick204nqh@gmail.com>
2020-05-13 15:21:20 +0700

    set proxy for client (attention)

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <div className='container mt-4'>
+      <h4 className='display-4 text-center mb-4'>
+        <i className='fab fa-react'></i> React File Upload
+      </h4>
+    </div>
+  );
 }
 
 export default App;
