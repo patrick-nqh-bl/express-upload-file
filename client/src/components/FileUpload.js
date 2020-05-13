35b26a55c957a36fbee74d7774d831f8c71d611f
Patrick <patrick204nqh@gmail.com>
2020-05-14 01:49:10 +0700

    bug: state being reset when submit form upload file

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import Message from './Message';
 import axios from 'axios';
 
 const FileUpload = () => {
   const [file, setFile] = useState('');
   const [filename, setFilename] = useState('Choose File');
   const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
 
   const onChange = (e) => {
     setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
+    console.log(e.target.files[0]);
   };
 
   const onSubmit = async (e) => {
@@ -26,17 +29,20 @@ const FileUpload = () => {
       const { fileName, filePath } = res.data;
 
       setUploadedFile({ fileName, filePath });
+
+      setMessage('File Uploaded');
     } catch (err) {
       if (err.response.status === 500) {
-        console.log('There was a problem with the server');
+        setMessage('There was a problem with the server');
       } else {
-        console.log(err.response.data.msg);
+        setMessage(err.response.data.msg);
       }
     }
   };
 
   return (
     <>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className='custom-file'>
           <input
